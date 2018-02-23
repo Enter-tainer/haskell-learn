6e473cc29cf6098c3eb4a5c16e94eb08a439c89e
Margatroid <351652632@qq.com>
2018-02-23 12:24:06 +0800

    add better version

--- codewars/6kyu/array.diff.hs
+++ codewars/6kyu/array.diff.hs
@@ -7,3 +7,9 @@ difference :: Eq a => [a] -> [a] -> [a]
 difference a [] = a
 difference [] _ = []
 difference a (x:xs) = difference (mydiff a x) xs
+
+-- A better version 
+difference a b = filter (`notElem` b) a
+
+-- A better version I write
+difference a b = [qwq | qwq <- a, qwq `notElem` b]
