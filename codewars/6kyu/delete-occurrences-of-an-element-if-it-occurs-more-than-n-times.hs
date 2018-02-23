5833d71d2735ee5a67ac3dc7d081c876da9e2ed6
Margatroid <351652632@qq.com>
2018-02-23 21:56:36 +0800

    AC

--- codewars/6kyu/delete-occurrences-of-an-element-if-it-occurs-more-than-n-times.hs
+++ codewars/6kyu/delete-occurrences-of-an-element-if-it-occurs-more-than-n-times.hs
@@ -1,4 +1,8 @@
 module Codewars.Kata.Deletion where
-
+work :: [Int] -> [Int] -> Int -> [Int]
+work _ [] _ = []
+work pre suc n
+    | length [qwq | qwq <- head suc : pre, qwq == head suc] <= n = head suc : work (pre ++ [head suc]) (tail suc) n
+    | otherwise = work (pre ++ [head suc]) (tail suc) n
 deleteNth :: [Int] -> Int -> [Int]
-deleteNth lst n = error "todo: deleteNth"
+deleteNth = work [] 
