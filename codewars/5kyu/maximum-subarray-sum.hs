55fd07ef12a59fa5771b7da7ee641056161d18aa
Margatroid <351652632@qq.com>
2018-02-24 21:34:29 +0800

    use scanl

--- codewars/5kyu/maximum-subarray-sum.hs
+++ codewars/5kyu/maximum-subarray-sum.hs
@@ -1,4 +1,4 @@
 module MaxSequence where
 
 maxSequence :: [Int] -> Int
-maxSequence s = foldl max 0 (foldl (\acc x -> if head acc + x < 0 then 0:acc else head acc + x:acc) [0] s)
+maxSequence = maximum . scanl (\acc x -> max 0 acc + x) 0
