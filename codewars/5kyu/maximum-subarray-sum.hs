80a05c8089110393a3319e83446243e26d6a0d6e
Margatroid <351652632@qq.com>
2018-02-24 20:38:16 +0800

    a better ver

--- codewars/5kyu/maximum-subarray-sum.hs
+++ codewars/5kyu/maximum-subarray-sum.hs
@@ -1,6 +1,4 @@
 module MaxSequence where
 
 maxSequence :: [Int] -> Int
-maximum' [] = 0
-maximum' x = maximum x
-maxSequence s = maximum' (foldl (\acc x -> if head acc + x < 0 then 0:acc else head acc + x:acc) [0] s)
+maxSequence s = foldl max 0 (foldl (\acc x -> if head acc + x < 0 then 0:acc else head acc + x:acc) [0] s)
