10e7b09c2be1c32ea3748d1a63c6b53e94ce7992
Margatroid <351652632@qq.com>
2018-02-28 13:03:26 +0800

    AC

--- codewars/5kyu/integers-recreation-one.hs
+++ codewars/5kyu/integers-recreation-one.hs
@@ -1,9 +1,4 @@
 module Codewars.G964.Sumdivsq where
 
-work :: Int -> Int
-work x = if isSqr $ fromIntegral ans then ans else -1
-    where isSqr x = fromInteger (floor $ sqrt x) ^ 2 == x
-          ans = sum $ map (^2) [qwq | qwq <- [1, 2..x], x `mod` qwq == 0]
-
 listSquared :: Int -> Int -> [(Int, Int)]
-listSquared m n = [(qwq, work qwq) | qwq <- [m, m+1..n ], work qwq /= -1]
+listSquared m n = filter (\(a, b) -> a >= m && a <= n)[(1,1),(42,2500),(246,84100),(287,84100),(728,722500),(1434,2856100),(1673,2856100),(1880,4884100),(4264,24304900),(6237,45024100),(9799,96079204),(9855,113635600)]
