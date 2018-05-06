0feaad6fefd4cb7fd15a0f43d951c0060500b8c0
Margatroid <351652632@qq.com>
2018-05-06 12:53:00 +0800

    TLE again

--- codewars/3kyu/last-digit-of-a-huge-number.hs
+++ codewars/3kyu/last-digit-of-a-huge-number.hs
@@ -1,5 +1,12 @@
 {-# OPTIONS_GHC -optc-O3 #-}
 module LastDigit (lastDigit) where
-import qualified Data.Vector as V
+
+fastPow :: Integer -> Integer -> Integer -> Integer
+fastPow base 1 m = mod base m
+fastPow base 0 m = 1
+fastPow base pow m | even pow = mod ((fastPow base (div pow 2) m) ^ 2) m
+                    | odd  pow = mod ((fastPow base (div (pow-1) 2) m) ^ 2 * base) m
+
 lastDigit :: [Integer] -> Integer
-lastDigit xs = (V.foldr' (\x acc -> x ^ (if acc < 4 then acc else acc `mod` 4 + 4)) 1 $ V.fromList xs) `mod` 10
+lastDigit [] = 1
+lastDigit xs = (foldr1 (\x acc -> fastPow x acc 9982443531926081723333333333000) xs) `mod` 10
