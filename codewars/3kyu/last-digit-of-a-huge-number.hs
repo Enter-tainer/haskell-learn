6aabd9e734c7cdbc68fdf279cdcf7d399242c067
Margatroid <351652632@qq.com>
2018-05-06 12:53:18 +0800

    fix

--- codewars/3kyu/last-digit-of-a-huge-number.hs
+++ codewars/3kyu/last-digit-of-a-huge-number.hs
@@ -5,7 +5,7 @@ fastPow :: Integer -> Integer -> Integer -> Integer
 fastPow base 1 m = mod base m
 fastPow base 0 m = 1
 fastPow base pow m | even pow = mod ((fastPow base (div pow 2) m) ^ 2) m
-                    | odd  pow = mod ((fastPow base (div (pow-1) 2) m) ^ 2 * base) m
+                   | odd  pow = mod ((fastPow base (div (pow-1) 2) m) ^ 2 * base) m
 
 lastDigit :: [Integer] -> Integer
 lastDigit [] = 1
