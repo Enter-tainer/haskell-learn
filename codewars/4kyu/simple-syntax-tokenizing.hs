39f65c6263b38c21811475f096ff57c3a3f603d0
Margatroid <351652632@qq.com>
2018-07-01 20:25:25 +0800

    TLE QAQ

--- codewars/4kyu/simple-syntax-tokenizing.hs
+++ codewars/4kyu/simple-syntax-tokenizing.hs
@@ -6,23 +6,23 @@ data Token = Token String | Brackets [Token]
 
 tokenize x
   | isValidBracketsSeq x == False = Nothing
-  | otherwise = Just $ mtokenize $ trim x
+  | otherwise = Just $ mtokenize $ x
 
 mtokenize :: String -> [Token]
 mtokenize str@(x:xs) 
   | x == '(' = (Brackets $ mtokenize $ realPre) 
-               : (mtokenize $ trim suc)
+               : (mtokenize $ trimStart suc)
   | otherwise = let (now, rem) = span (\y -> isSame x y) str
-                in (Token $ trim now) : (mtokenize $ trim rem)
+                in (Token $ trimEnd now) : (mtokenize $ trimStart rem)
   where cnt qwq = length . filter (==qwq)
-        pre = head $ tail $ filter (\x -> (cnt '(' x) == (cnt ')' x)) $ inits str
+        _:pre:_ = filter (\x -> (cnt '(' x) == (cnt ')' x)) $ inits str
         suc = drop (length pre) str
-        realPre = init $ tail $ trim pre
+        realPre = init $ tail pre
 mtokenize [] = []
 
 isValidBracketsSeq :: String -> Bool
-isValidBracketsSeq raw_str = leftCnt raw_str == rightCnt raw_str &&
-  (allTrue $ map (\x -> leftCnt x >= rightCnt x) $ inits raw_str)
+isValidBracketsSeq rawStr = leftCnt rawStr == rightCnt rawStr &&
+  (allTrue $ map (\x -> leftCnt x >= rightCnt x) $ inits rawStr)
   where leftCnt = length . filter (=='(')
         rightCnt = length . filter (==')')
         allTrue (x:xs) = x && allTrue xs
@@ -33,5 +33,10 @@ isSame x y = (isOp x && isOp y) || (isLetter x && isLetter y)
   where isOp qwq = qwq `elem` "!#$%&*+-/<=>@^_.,;"
 
 trim :: String -> String
-trim = f . f
-  where f = reverse . dropWhile isSpace
+trim = trimStart . trimEnd
+
+trimStart :: String -> String
+trimStart = dropWhile isSpace
+
+trimEnd :: String -> String
+trimEnd = reverse . dropWhile isSpace . reverse
