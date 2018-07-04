812eb9c4cd7d2827a56273620d9489b08806b084
Margatroid <351652632@qq.com>
2018-07-04 13:23:00 +0800

    memover

--- codewars/5kyu/memoized-fibonacci.hs
+++ codewars/5kyu/memoized-fibonacci.hs
@@ -1,12 +1,12 @@
 module Fibonacci where
--- import Data.Vector
+import Data.Vector
 
--- fibonacci :: Int -> Integer
--- fibonacci n = v ! (n) where
---     v = generate (n+1) f
---     f 0 = 0
---     f 1 = 1
---     f k = (v ! (k-1)) + (v ! (k-2))
+fibonacci :: Int -> Integer
+fibonacci n = v ! (n) where
+    v = generate (n+1) f
+    f 0 = 0
+    f 1 = 1
+    f k = (v ! (k-1)) + (v ! (k-2))
 
 fib' :: Int -> Integer
 fib' n = fibVec !! n
