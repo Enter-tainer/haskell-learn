1db15edb9f337f693d2030b5c414d678a8458a69
Margatroid <i@margatroid.xyz>
2019-08-16 10:29:58 +0800

    feat: add while loop

---
+++ whileloop.hs
@@ -0,0 +1,25 @@
+import           Control.Monad
+import           Control.Monad.Trans
+import           Control.Monad.Trans.Maybe
+import           Data.IORef
+import           Text.Printf
+
+while :: (Monad m) => m Bool -> m a -> m ()
+while cond_ stat = do
+  cond <- cond_
+  when cond $ do
+    _ <- stat
+    while cond_ stat
+
+loop :: Monad m => MaybeT m a -> m (Maybe b)
+loop stat = runMaybeT $ forever stat
+
+main :: IO ()
+main = do
+  i <- newIORef (1::Int)
+  _ <- loop $ do
+    j <- lift $ readIORef i
+    lift $ printf "%d\n" j
+    lift $ modifyIORef' i (+1)
+    when (j == 100) mzero
+  return ()
