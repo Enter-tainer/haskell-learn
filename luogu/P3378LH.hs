75a9480c7ad15d37d1682220465e740268377248
Margatroid <i@margatroid.xyz>
2020-07-26 11:06:27 +0800

    format code

--- luogu/P3378LH.hs
+++ luogu/P3378LH.hs
@@ -1,10 +1,12 @@
 {-# OPTIONS_GHC -O2 #-}
+
 -- {-# language Strict #-}
 module P3378LH where
+
 import Data.Char (digitToInt, isSpace)
-import Text.Printf (printf)
 import qualified Data.Text as T
 import qualified Data.Text.IO as I
+import Text.Printf (printf)
 
 repM :: Monad m => Int -> a -> (a -> m a) -> m a
 repM 0 x _ = return x
@@ -17,8 +19,8 @@ int :: String -> Int
 int str = int' (filter (not . isSpace) str) 0
   where
     int' [] x = x
-    int' ('-':xs) _ = -1 * (int' xs 0)
-    int' (x:xs) p = int' xs $ p * 10 + digitToInt x
+    int' ('-' : xs) _ = -1 * (int' xs 0)
+    int' (x : xs) p = int' xs $ p * 10 + digitToInt x
 
 data Heap a = Empty | Heap a Int (Heap a) (Heap a)
 
@@ -40,12 +42,15 @@ infixl 5 ><
 Empty >< v = v
 u >< Empty = u
 u@(Heap uval udis ulc urc) >< v@(Heap vval vdis vlc vrc)
-  | uval <= vval = let x = urc >< v in
-                     if dis ulc < dis x
-                     then Heap uval (dis ulc + 1) x ulc
-                     else Heap uval (dis x + 1) ulc x
+  | uval <= vval =
+    let x = urc >< v
+     in if dis ulc < dis x
+          then Heap uval (dis ulc + 1) x ulc
+          else Heap uval (dis x + 1) ulc x
   | otherwise = v >< u
+
 infixl 5 <|
+
 (<|) :: (Ord a) => Heap a -> a -> Heap a
 tr <| val = tr >< Heap val 1 Empty Empty
 
@@ -53,15 +58,15 @@ deleteMin :: (Ord a) => Heap a -> Heap a
 deleteMin Empty = error "Cannot delete anything from empty heap"
 deleteMin (Heap _ _ lc rc) = lc >< rc
 
-
 main :: IO ()
 main = do
   n <- int <$> T.unpack <$> I.getLine
   repM_ n (Empty) $ \(root) -> do
-    (x:xs) <- map (int . T.unpack) . T.words <$> I.getLine
-    case x of 1 -> do
-                     let num:[] = xs
-                     return (root <| num)
-              2 -> printf "%d\n" (findMin root) >> return (root)
-              3 -> return (deleteMin root)
-              _ -> error "nmsl"
+    (x : xs) <- map (int . T.unpack) . T.words <$> I.getLine
+    case x of
+      1 -> do
+        let num : [] = xs
+        return (root <| num)
+      2 -> printf "%d\n" (findMin root) >> return (root)
+      3 -> return (deleteMin root)
+      _ -> error "nmsl"
